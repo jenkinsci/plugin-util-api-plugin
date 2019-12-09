0288f545926a9a0456da90fbec36d976e1147ff8
Ulli Hafner <ullrich.hafner@gmail.com>
2019-12-09 10:50:09 +0100

    Deploy plugin to JENKINS_HOME after build.

--- go.sh
+++ go.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
+JENKINS_HOME=../docker/volumes/jenkins-home
+
 mvn clean install || { echo "Build failed"; exit 1; }
 
+echo "Installing plugin in $JENKINS_HOME"
+
+rm -rf $JENKINS_HOME/plugins/plugin-util-api-plugin*
+cp -fv target/plugin-util-api.hpi $JENKINS_HOME/plugins/plugin-util-api.jpi
+
+CURRENT_UID="$(id -u):$(id -g)"
+export CURRENT_UID
+IS_RUNNING=$(docker-compose ps -q jenkins-master)
+if [[ "$IS_RUNNING" != "" ]]; then
+    docker-compose restart
+    echo "Restarting Jenkins (docker compose with user ID ${CURRENT_UID}) ..."
+fi
