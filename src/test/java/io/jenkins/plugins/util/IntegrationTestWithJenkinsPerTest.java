4cae27c1e563af7469f5b86f507cf6f2c7a1fff9
Ulli Hafner <ullrich.hafner@gmail.com>
2022-02-14 22:59:39 +0100

    Remove HTML-Unit helper methods.

--- src/test/java/io/jenkins/plugins/util/IntegrationTestWithJenkinsPerTest.java
+++ src/test/java/io/jenkins/plugins/util/IntegrationTestWithJenkinsPerTest.java
@@ -1,9 +1,7 @@
 package io.jenkins.plugins.util;
 
-import org.junit.Before;
 import org.junit.Rule;
 import org.jvnet.hudson.test.JenkinsRule;
-import org.jvnet.hudson.test.JenkinsRule.WebClient;
 
 /**
  * Base class for integration tests in Jenkins. Sub classes will get a new and fresh Jenkins instance for each
@@ -19,20 +17,5 @@ public abstract class IntegrationTestWithJenkinsPerTest extends IntegrationTest 
     @Override
     protected JenkinsRule getJenkins() {
         return jenkinsPerTest;
-    }
-
-    private WebClient noJsWebClient;
-    private WebClient jsEnabledClient;
-
-    /** Creates two web clients, one with and one without JS support. */
-    @Before
-    public void createWebClients() {
-        noJsWebClient = create(jenkinsPerTest, false);
-        jsEnabledClient = create(jenkinsPerTest, true);
-    }
-
-    @Override
-    protected WebClient getWebClient(final JavaScriptSupport javaScriptSupport) {
-        return javaScriptSupport == JavaScriptSupport.JS_DISABLED ? noJsWebClient : jsEnabledClient;
     }
 }
