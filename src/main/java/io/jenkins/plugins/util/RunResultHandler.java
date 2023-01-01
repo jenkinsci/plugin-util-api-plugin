423a11fe4d6ed9bcf01f54ddffbef697377e3f82
Ulli Hafner <ullrich.hafner@gmail.com>
2023-01-01 22:35:26 +0100

    Move `StageResultHandler` abstraction from warnings plugin.
    A `StageResultHandler` can set the stage or build result.
    Implementations for freestyle jobs and pipelines are provided.

---
+++ src/main/java/io/jenkins/plugins/util/RunResultHandler.java
@@ -0,0 +1,28 @@
+package io.jenkins.plugins.util;
+
+import hudson.model.Result;
+import hudson.model.Run;
+
+/**
+ * {@link StageResultHandler} that sets the overall build result of the {@link Run}.
+ *
+ * @author Devin Nusbaum
+ */
+public class RunResultHandler implements StageResultHandler {
+    private final Run<?, ?> run;
+
+    /**
+     * Creates a new instance of {@link RunResultHandler}.
+     *
+     * @param run
+     *         the run to set the result for
+     */
+    public RunResultHandler(final Run<?, ?> run) {
+        this.run = run;
+    }
+
+    @Override
+    public void setResult(final Result result, final String message) {
+        run.setResult(result);
+    }
+}
