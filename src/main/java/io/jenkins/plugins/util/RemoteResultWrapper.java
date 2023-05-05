0b7e90ef117f731e8884279536f1c0abdf690d8a
Ulli Hafner <ullrich.hafner@gmail.com>
2023-05-05 08:05:00 +0200

    Make the URL in description a clickable URL.
    Moreover, cleanup the code.

---
+++ src/main/java/io/jenkins/plugins/util/RemoteResultWrapper.java
@@ -0,0 +1,55 @@
+package io.jenkins.plugins.util;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import edu.hm.hafner.util.FilteredLog;
+
+/**
+ * A serializable result combined with a logger. Enables remote calls to return a result and a corresponding log.
+ *
+ * @param <T>
+ *         the type of the result
+ *
+ * @author Ullrich Hafner
+ */
+public class RemoteResultWrapper<T extends Serializable> extends FilteredLog {
+    private static final long serialVersionUID = -6411417555105688927L;
+
+    private final T result;
+
+    /**
+     * Creates a new instance of {@link RemoteResultWrapper}.
+     *
+     * @param title
+     *         the title of the error messages
+     * @param result
+     *         the wrapped result
+     */
+    public RemoteResultWrapper(final T result, final String title) {
+        super(title);
+
+        this.result = result;
+    }
+
+    public T getResult() {
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        RemoteResultWrapper<?> that = (RemoteResultWrapper<?>) o;
+        return Objects.equals(result, that.result);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(result);
+    }
+}
---
+++ src/test/java/io/jenkins/plugins/util/RemoteResultWrapperTest.java
@@ -0,0 +1,26 @@
+package io.jenkins.plugins.util;
+
+import org.junit.jupiter.api.Test;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+
+import static org.assertj.core.api.Assertions.*;
+
+class RemoteResultWrapperTest {
+    @Test
+    void shouldCreateWrapper() {
+        var result = "result";
+
+        var wrapper = new RemoteResultWrapper<>(result, "title");
+
+        assertThat(wrapper.getResult()).isEqualTo(result);
+
+        wrapper.logInfo("Hello %s", "World");
+        assertThat(wrapper.getInfoMessages()).containsExactly("Hello World");
+    }
+
+    @Test
+    void shouldAdhereToEquals() {
+        EqualsVerifier.forClass(RemoteResultWrapper.class).withRedefinedSuperclass().verify();
+    }
+}
